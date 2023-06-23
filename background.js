dfed4509143d22caf3e75724b265542b4a16fd77
thismarcoantonio <thismarcoantonio@gmail.com>
2023-06-23 15:07:31 -0300

    Remove on off badge

--- background.js
+++ background.js
@@ -1,30 +1,16 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: "OFF",
-  });
-});
+let isOpen = false;
 
 chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url.includes("patreon.com")) {
-    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === "ON" ? "OFF" : "ON";
-
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
-
-    if (nextState === "ON") {
+    if (!isOpen) {
+      isOpen = true;
       await chrome.scripting.executeScript({
         world: "MAIN",
         target: { tabId: tab.id, allFrames: true },
         files: ["content.js"],
       });
     }
-    if (nextState === "OFF") {
+    if (isOpen) {
       await chrome.scripting.executeScript({
         world: "MAIN",
         target: { tabId: tab.id, allFrames: true },
