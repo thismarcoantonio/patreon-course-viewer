37c970260d2913b19535d35b06c77427edf7b5c3
thismarcoantonio <thismarcoantonio@gmail.com>
2023-06-30 23:54:51 -0300

    Track if active url is valid

--- background.js
+++ background.js
@@ -1,7 +1,15 @@
 let isOpen = false;
+let isCurrentUrlValid = false;
+
+function isValidUrl(url) {
+  return (
+    url.startsWith("https://www.patreon.com") ||
+    url.startsWith("https://patreon.com")
+  );
+}
 
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.includes("patreon.com")) {
+  if (isValidUrl(tab.url)) {
     if (!isOpen) {
       isOpen = true;
       await chrome.scripting.executeScript({
@@ -18,4 +26,20 @@ chrome.action.onClicked.addListener(async (tab) => {
       });
     }
   }
+});
+
+chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+  const tab = await chrome.tabs.get(tabId);
+  if (isCurrentUrlValid === isValidUrl(tab.url)) return;
+
+  isCurrentUrlValid = !isCurrentUrlValid;
+
+  chrome.action.setIcon({
+    path: {
+      16: `public/16${!isCurrentUrlValid ? "-grey" : ""}.png`,
+      32: `public/32${!isCurrentUrlValid ? "-grey" : ""}.png`,
+      48: `public/48${!isCurrentUrlValid ? "-grey" : ""}.png`,
+      128: `public/128${!isCurrentUrlValid ? "-grey" : ""}.png`,
+    },
+  });
 });
